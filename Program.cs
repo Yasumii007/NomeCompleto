1509f7eed5efd88723eda4726e37cb007e0ab328
Yasumii007 <yasmimalvesrodrigues16@gmail.com>
2025-03-26 20:34:37 -0300

    Atividade nome completo, que mostra o nome completo dela e o seu nome de catalogo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string nome, sobrenome;
+
+Console.WriteLine("Por favor digite seu nome primeiro nome");
+nome = Console.ReadLine()!;
+
+Console.WriteLine("Por favor digite seu sobrenome");
+
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Seu nome completo é: {nome} {sobrenome}");
+
+Console.WriteLine($"Seu nome de catalogo é: {sobrenome.ToUpper()}, {nome}");
